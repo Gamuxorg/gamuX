c91e01d8f17f73f33db9fcf856a368919c30a9e2
Demon <hyh19962008@gmail.com>
2020-12-02 20:11:53 +0800

    轮播图片api增加文章链接

--- api/images.php
+++ api/images.php
@@ -7,50 +7,59 @@
 	 */
 	class MainSlidePic {
 		private $postTotalCount;
-		private $postOffsetArr;
+		public $post_idUsed;		//已经使用或判定无效的文章id
 		public $srcs;
+		public $post_ids;			//最终使用的文章id
+		public $posts;				//返回 postLink + src
 
 		public function __construct() {
 			$this->postTotalCount = wp_count_posts()->publish;
-			$this->postOffsetArr = array();
+			$this->post_idUsed = array();
 			$this->srcs = array();
+			$this->post_ids = array();
+			$this->posts = array();
 		}
 
 		/**
-		 * 返回一个不重复的随机的文档下标
+		 * 返回一个不重复的随机的文档ID
 		 *
-		 * @return int $offset
+		 * @return int $id
 		 */
-		private function randOffset() : int {
+		private function randID() : int {
 			do {
-				$offset = rand(0, $this->postTotalCount - 1);
-			}while(in_array($offset, $this->postOffsetArr));
+				$id = rand(0, $this->postTotalCount - 1);
+			}while(in_array($id, $this->post_idUsed));
 			
-			array_push($this->postOffsetArr, $offset);
-			return $offset;
+			array_push($this->post_idUsed, $id);
+			return $id;
 		}
 
 		/**
 		 * 获取一个游戏内的所有图片
 		 *
-		 * @return mixed $imgs || NULL
+		 * @return mixed $post || NULL
 		 */
 		private function newImages() {
 			do {
-				$offset = $this->randOffset();
+				$id = $this->randID();
+				while(\get_post_type($id) != 'post')
+					$id = $this->randID();
 				$post = get_posts([
-					'numberposts'=>1,
-					'offset'=>$offset,
+					'numberposts'=> 1,
+					'offset'=> $id,
 					'category'=> [
-						1				//只获取游戏类别
+						256					//只获取游戏类别
 					]
 				]);
-			}while(empty($post));		//如果offset是非游戏类别时会返回空
-
+			}while(empty($post));			//如果offset是非游戏类别时会返回空
+			
 			$content = $post[0]->post_content;
 			$imgCount = preg_match_all('/<img.*\/>/', $content, $imgs);
-			if($imgCount>0){
-				return $imgs[0];
+			if($imgCount > 0) {
+				$post = new \stdClass();
+				$post->id = $id;
+				$post->imgs = $imgs[0];
+				return $post;
 			}
 			else
 				return NULL;			//有些游戏没有图片
@@ -60,7 +69,7 @@
 		 * 获取一个游戏内的一张随机的图片的URL
 		 *
 		 * @param Array $imgs
-		 * @return bool
+		 * @return mixed $tmp
 		 */
 		private function imageSrc(Array $imgs) {
 			$arrLen = count($imgs);
@@ -70,30 +79,36 @@
 			if($count > 0){
 				$tmp = substr($tmp[0][0], 5);				//删除src="
 				$tmp = substr($tmp, 0, strlen($tmp)-1);		//删除最后的"
-				array_push($this->srcs, $tmp);
-				return true;
+				return $tmp;
 			}
 			else
 				return false;				//有些img标签里没src属性
 		}
 
 		/**
-		 * 获取制定数量的图片的链接src
+		 * 获取指定数量的图片的链接src及其文章链接
 		 *
 		 * @param int $picNum
-		 * @return Array $srcs
+		 * @return Array $posts
 		 */
-		public function getImageSrcs($picNum = 4) {
+		public function getImageSrcs($picNum = 4) : array {
 			for($i = 0; $i < $picNum; $i++) {
 				do {
-					$imgs = $this->newImages();
-				}while(is_null($imgs));
+					$post = $this->newImages();
+				}while(is_null($post));
+				array_push($this->post_ids, $post->id);
 
 				do {
-					$src = $this->imageSrc($imgs);
-				}while(!$src);
+					$src = $this->imageSrc($post->imgs);
+				}while($src === false);
+				array_push($this->srcs, $src);
+
+				array_push($this->posts, array(
+					"postLink" => get_permalink($this->post_ids[$i]),
+					"imageSrc" => $this->srcs[$i]
+				));
 			}
-			return $this->srcs;
+			return $this->posts;
 		}
 	} //class
 
