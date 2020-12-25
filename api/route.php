4bbc75e8cf869d30a406c9a3c6b9cdcfb41eeef8
Demon <hyh19962008@gmail.com>
2020-12-25 20:51:02 +0800

    修复wp5.6中的API权限问题

--- api/route.php
+++ api/route.php
@@ -4,6 +4,9 @@
 //扩展 wp-json/wp/v2/posts/<post_id>，添加自定义的字段
 include("posts_ext.php");
 
+//扩展 wp-json/wp/v2/users/<user_id>，添加未阅读文章数
+include("user_comment.php");
+
 //获取首页轮播图片 ，调用get_mainSlide
 //route: wp-json/gamux/v1/images/mainslide/<picnum>?
 include("images.php");
@@ -11,6 +14,7 @@ add_action( 'rest_api_init', function () {
 	register_rest_route( 'gamux/v1', '/images/mainslide(/?)(?P<picnum>\d*)', array(
 		'methods' => 'GET',
 		'callback' => '\Gamux\get_mainSlide',
+		'permission_callback' => "__return_true",
 		'args' => array(
 			'picnum' => array(
 				'default' => 4,
@@ -25,6 +29,18 @@ add_action( 'rest_api_init', function () {
 	));
 });
 
+add_action('rest_api_init', function () {
+	register_rest_route( 'gamux/v1', '/test', array(
+		'methods' => 'GET',
+		'callback' => function(WP_REST_Request $request){
+			
+			wp_die(new \WP_Error(500, "致命错误"));
+			// return 0;
+		},
+		'permission_callback' => "__return_true"
+	));	
+});
+
 //获取评论，输入post_id，调用get_comments
 //route: wp-json/gamux/v1/comments/(?P<post>\d+)
 include("comments.php");
@@ -32,6 +48,7 @@ add_action( 'rest_api_init', function () {
 	register_rest_route( 'gamux/v1', '/comments/(?P<post>\d+)', array(
 		'methods' => 'GET',
 		'callback' => '\Gamux\get_comments',
+		'permission_callback' => "__return_true",
 		'args' => array(
 			'post' => array(
 				'validate_callback' => function($param, $request, $key) {
@@ -46,12 +63,13 @@ add_action( 'rest_api_init', function () {
 });
 
 //获取用户未读评论数，输入user_id
-//route: wp-json/gamux/v1/comments/unread/(?P<user>\d+)
-include("user_comment.php");
+// route: wp-json/gamux/v1/comments/unread/(?P<user>\d+)
+include_once("user_comment.php");
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'gamux/v1', '/comments/unread/(?P<user>\d+)', array(
 		'methods' => 'GET',
 		'callback' => '\Gamux\get_user_unread',
+		'permission_callback' => "__return_true",
 		'args' => array(
 			'user' => array(
 				'validate_callback' => function($param, $request, $key) {
@@ -64,5 +82,6 @@ add_action( 'rest_api_init', function () {
 		)
 	));
 });
+
 
 ?>
--- function/oauth2_route.php
+++ function/oauth2_route.php
@@ -4,20 +4,23 @@
 add_action('rest_api_init', function () {
 	register_rest_route( 'gamux/v1', '/oauth/github', array(
 		'methods' => 'GET',
-		'callback' => '\Gamux\github_oauth_login_init'
+		'callback' => '\Gamux\github_oauth_login_init',
+		'permission_callback' => '__return_true'
 	));
 });
 
 add_action('rest_api_init', function () {
 	register_rest_route( 'gamux/v1', '/oauth/weibo', array(
 		'methods' => 'GET',
-		'callback' => '\Gamux\weibo_oauth_login_init'
+		'callback' => '\Gamux\weibo_oauth_login_init',
+		'permission_callback' => '__return_true'
 	));
 });
 
 add_action('rest_api_init', function () {
 	register_rest_route( 'gamux/v1', '/oauth/qq', array(
 		'methods' => 'GET',
-		'callback' => '\Gamux\qq_oauth_login_init'
+		'callback' => '\Gamux\qq_oauth_login_init',
+		'permission_callback' => '__return_true'
 	));
 });
