e45482ddf29ff0ceac94fcb04b9587c047954b2e
xiaoji <345865759@163.com>
2020-12-27 23:38:59 +0800

    fix bug

---
+++ plugins/gamux-slide-url/src/edit.js
@@ -0,0 +1,62 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+import { TextControl, Button } from '@wordpress/components';
+import { createBlock } from '@wordpress/blocks';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+const BACKGROUND_COLORS = {
+	grey: '#f3f4f5',
+    green: '#e9fbe5'
+};
+
+export default function Edit( { 
+    attributes,
+	setAttributes,
+	insertBlocksAfter,
+	isSelected } ) {
+    return (
+        <div { ...useBlockProps() } >
+            <TextControl 
+                value={ attributes.content }
+                onChange={ ( val ) => setAttributes( { content: val } ) }
+            />  
+            <Button 
+                isSecondary 
+                isSmall
+                className="addSlideUrl-botton"
+                onClick={ (e) => {
+                    e.preventDefault();
+                    insertBlocksAfter( createBlock( 'gamux/slide-url' ) ); 
+                } }>
+                    添加轮播图URL
+            </Button>
+        </div>         
+    );
+}
