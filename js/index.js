e81c93864bf1209c2e932a1ad5bb4cac74d569b7
xiaoji <345865759@163.com>
2021-01-01 00:34:15 +0800

    首页游戏加载错误

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ var index = new Vue({
         method: 'get',
         url: this.siteurl + '/wp-json/wp/v2/categories',
         params: {
-          "parent": 112,
+          "parent": 256,
           "per_page": 100,
         },
       });
