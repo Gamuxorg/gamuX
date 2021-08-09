c647f89851844694cb79ccc5e9e8407fecb0751e
zhaozhen <345865759@163.com>
2021-08-09 21:54:00 +0800

    实现游戏页参数功能

--- js/category.js
+++ js/category.js
@@ -1,84 +1,159 @@
+//全局变量一律大写，局部变量和函数内部变量一律小写，函数名驼峰
 var category = new Vue({
-  el: '#section',
-  data: {
-    postdata: [],
-    siteurl: "",
-    currentpage: 1,
-    total: 0,
-    categories: [],
-    cateidlist: [],
-    thiscateid: 0,
-    param: 0,
-  },
-  methods: {
-    refresh: function() {
-      location.reload(false);
+    el: '#section',
+    data: {
+        //所有子分类信息
+        CATEGORIES: [],
+        //单个文章的信息
+        POSTDATA: [],
+        SITEURL: null,
+        //当前页的码数
+        CURRENTPAGE: null,
+        //当前分类ID
+        CURRENTID: null,
+        //当前分类下的所有文章数
+        PAGISHOW: false,
+        CATSHOW: false,
+        TOTAL: 0,
+        //文章列表
+        POSTLIST: [],
+        CATID: null,
+        PARAM: 0,
     },
-    clickCat: async function(e) {
-      this.thiscateid = e.currentTarget.id;
-      const a = await axios({
-        method: 'get',
-        url: this.siteurl + '/wp-json/wp/v2/posts',
-        params: {
-          "per_page": 20,
-          "categories": [this.thiscateid],
+    methods: {
+        refresh: function() {
+            window.location.href = this.SITEURL + "/gamelist";
         },
-      });
-      this.postdata = a.data;
-      this.total = this.postdata.length;    
-    },
-    handleCurrentChange: async function (val) {
-      if (this.thiscateid == 0) {
-        var cat = this.cateidlist;
-      }
-      else {
-        var cat = this.thiscateid;
-      }
-      const a = await axios({
-        method: 'get',
-        url: this.siteurl + '/wp-json/wp/v2/posts',
-        params: {
-          "per_page": 20,
-          "page": val,
-          "categories": cat,
+        queryParse: function() {
+            var queryParse = location.href;
+            try {
+                queryParse = queryParse.replace("?", "?&").split("&");
+                var queryParsec = [];
+                queryParsec = {
+                    "cat": Number((queryParse[1].split("="))[1]),
+                    "page": Number((queryParse[2].split("="))[1])
+                }
+            } catch {
+                queryParsec = {
+                    "cat": 256,
+                    "page": 1
+                }
+            }
+            return queryParsec;
         },
-      });
-      this.postdata = a.data;
-    },
-    getCatJson: async function() {
-      const a = await axios({
-        method: 'get',
-        url: this.siteurl + '/wp-json/wp/v2/categories',
-        params: {
-          "parent": 256,
-          "per_page": 100,
+        clickCat: async function(e) {
+            this.CURRENTID = e.currentTarget.id;
+            const a = await axios({
+                method: 'get',
+                url: this.SITEURL + '/wp-json/wp/v2/posts',
+                params: {
+                    "per_page": 20,
+                    "categories": this.CURRENTID
+                },
+            });
+            this.CURRENTPAGE = 1;
+            this.POSTDATA = a.data;
+            this.TOTAL = Number(a["headers"]["x-wp-total"]);
+            window.history.pushState(null, null, this.SITEURL + "/gamelist?cat=" + this.CURRENTID + "&page=" + this.CURRENTPAGE);
         },
-      });
-      return a.data;
-    },
-    getPostJson: async function() {
-      const a = await axios({
-        method: 'get',
-        url: this.siteurl + '/wp-json/wp/v2/posts',
-        params: {
-          "per_page": 20,
-          "categories": this.cateidlist,
+        handleCurrentChange: async function(val) {
+            this.CURRENTPAGE = val;
+            if (this.CURRENTID == 256) {
+                var handleCurrentChange_param = {
+                    "per_page": 20,
+                    "include": this.POSTLIST.slice(20 * (this.CURRENTPAGE - 1), this.CURRENTPAGE * 20 - 1).join(',')
+                }
+            } else {
+                var handleCurrentChange_param = {
+                    "page": val,
+                    "per_page": 20,
+                    "categories": this.CURRENTID
+                };
+            }
+            const a = await axios({
+                method: 'get',
+                url: this.SITEURL + '/wp-json/wp/v2/posts',
+                params: handleCurrentChange_param,
+            });
+            this.POSTDATA = a.data;
+            console.log(this.POSTDATA);
+            window.history.pushState(null, null, this.SITEURL + "/gamelist?cat=" + this.CURRENTID + "&page=" + this.CURRENTPAGE);
         },
-      });
-      return a.data;
-    },
-  },
-  mounted: async function() {
-    this.siteurl = gamux.siteurl;
-    const cat = await this.getCatJson();
-    this.categories = cat;
-    for (var i = 0; i < cat.length; i++) {
-      this.cateidlist[i] = cat[i]['id'];
-      this.total = this.total + cat[i]['count'];
-    }
-    this.$nextTick( async function() {
-      this.postdata = await this.getPostJson();
-    })
+        //获取子分类信息
+        getCatJson: async function() {
+            const a = await axios({
+                method: 'get',
+                url: this.SITEURL + '/wp-json/wp/v2/categories',
+                params: {
+                    "parent": 256
+                },
+            });
+            return a.data;
+        },
+        //获取分类下所有文章的id
+        getPostJson_gamux: async function() {
+            const a = await axios({
+                method: 'get',
+                url: this.SITEURL + '/wp-json/gamux/v1/categories',
+                params: {
+                    "catid": this.CURRENTID
+                },
+            });
+            //返回值是一个包含符合要求的所有文章id的字符串
+            return a.data;
+        },
+        //初始化获取文章数据
+        getPostJson: async function() {
+            if (this.CURRENTID == 256) {
+                var getPostJson_param = {
+                    "per_page": 20,
+                    "include": this.POSTLIST.slice(20 * (this.CURRENTPAGE - 1), this.CURRENTPAGE * 20 - 1).join(',')
+                }
+            } else {
+                var getPostJson_param = {
+                    "per_page": 20,
+                    "categories": this.CURRENTID
+                };
+            }
+            const a = await axios({
+                method: 'get',
+                url: this.SITEURL + '/wp-json/wp/v2/posts',
+                params: getPostJson_param,
+            });
+            let b = a;
+            return b;
 
-  },
+        },
+    },
+    mounted: async function() {
+        this.SITEURL = gamux.siteurl;
+
+        this.CURRENTID = await this.queryParse().cat;
+        this.CURRENTPAGE = await this.queryParse().page;
+        this.CATEGORIES = await this.getCatJson();
+        if(this.CURRENTID == 256) {
+            catdata = await this.getPostJson_gamux();
+            //json中获取的posts的值是一个字符串，需要转化为数组
+            let postlistr = catdata.posts;
+            this.POSTLIST = postlistr.split(",");
+            //数组倒序，将新文章排在旧文章前面
+            this.POSTLIST = this.POSTLIST.reverse();
+            // 获取游戏的总数量
+            this.TOTAL = catdata.count;
+            let a = await this.getPostJson();
+            this.POSTDATA = a.data;
+        }
+        
+        if (this.CURRENTID != 256) {
+            let a = await this.getPostJson();
+            this.POSTDATA = a.data;
+            console.log(a);
+            this.TOTAL = Number(a["headers"]["x-wp-total"]);
+        }
+        this.$nextTick(async function() {
+            this.CATSHOW = true;
+            this.PAGISHOW = true;
+        });
+    },
+
 })
--- template/diy/gamelist.php
+++ template/diy/gamelist.php
@@ -8,21 +8,21 @@ get_header(); ?>
 <section id="index">
   <div id="catenamelist">
     <el-button type="text" id="games-all-button" @click="refresh">所有游戏</el-button>
-    <el-button type="text" v-for="cat in categories" :key="cat.id" :id="cat.id" @click="clickCat($event)">{{ cat.name }}</el-button>
+    <el-button type="text" v-for="cat in CATEGORIES" :key="cat.id" :id="cat.id" @click="clickCat($event)">{{ cat.name }}</el-button>
   </div>
 </section>
 
 <el-divider></el-divider>
 <!--列表-->
-<section id="category">
-    <el-card class="card" v-for="post in postdata" :key="post.id">
+<section id="category" v-if="CATSHOW">
+    <el-card class="card" v-for="post in POSTDATA" :key="post.id">
       <div class="pic">
       <a :href="post.link"><img :src="post.exts.thumbnail" class="image"></a>
       </div>
       <div class="game-info">
         <div class="game-name-sort">
           <div class="game-name"><a :href="post.link" v-html="post.title.rendered"></a></div>
-          <div class="game-sort"><el-button type="text"><a :href="siteurl + '/gamelist?catid=' + (post.exts.categories)[0].cat_id">{{ (post.exts.categories)[0].name }}</a></el-button></div>
+          <div class="game-sort"><el-button type="text"><a :href="SITEURL + '/gamelist?catid=' + (post.exts.categories)[0].cat_id">{{ (post.exts.categories)[0].name }}</a></el-button></div>
         </div>
         <div class="game-version-date">
           <!-- 无版本号则返回一个默认值 -->
@@ -36,14 +36,14 @@ get_header(); ?>
 
 <el-divider></el-divider>
 <!--翻页-->
-<section id="pagi">
+<section id="pagi" v-if="PAGISHOW">
   <div class="block">
     <el-pagination
       @current-change="handleCurrentChange"
-      :current-page.sync="currentpage"
+      :current-page.sync="CURRENTPAGE"
       :page-size="20"
       layout="total, prev, pager, next"
-      :total="total">
+      :total="TOTAL">
     </el-pagination>
   </div>
 </section>
