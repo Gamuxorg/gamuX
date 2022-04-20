44382d2c620af20cf804165eaf1460a5f42cca8b
xiaoji <345865759@163.com>
2022-04-20 22:46:44 +0800

    去除搜索页的搜索框

--- js/search.js
+++ js/search.js
@@ -1,59 +1,54 @@
 var category = new Vue({
-  el: '#section',
-  data: {
-    postdata: [],
-    siteurl: "",
-    currentpage: 1,
-    total: 0,
-    param: 0,
-    formInline: {
-      input: '',
-    },
-  },
-  methods: {
-    onSubmit: function() {
-      window.location.href = this.siteurl + "/search?s=" + this.formInline.input;
-    },
-    handleCurrentChange: function () {
-      const p = this.currentpage;
-      window.location.href = this.siteurl + "/search?s=" + this.getQueryvalue('s') + "&p=" + p;
-    },
-    getQueryvalue: function(name) {
-      var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-      var r = window.location.search.substr(1).match(reg);
-      if( r != null ) {
-        return  decodeURIComponent(r[2]);
-      }
-      return null;
-    },
-    getPostJson: async function() {
-      const e = this.getQueryvalue('s');
-      if (Number(this.getQueryvalue('p') == null)) {
-        var currentpage = 1
-      }
-      else {
-        var currentpage = Number(this.getQueryvalue('p'));
-      }
-      this.formInline.input = e;
-      const a = await axios({
-        method: 'get',
-        url: this.siteurl + '/wp-json/gamux/v1/search',
-        params: {
-          "page": currentpage,
-          "per_page": 10,
-          "search": e,
+    el: '#section',
+    data: {
+        postdata: [],
+        siteurl: "",
+        currentpage: 1,
+        total: 0,
+        param: 0,
+        formInline: {
+            input: '',
         },
-      });
-      return a;
-    }
-  },
-  mounted: async function() {
-    this.siteurl = gamux.siteurl;
-    const getJson = await this.getPostJson();
-    this.total = Number(getJson["headers"]["x-wp-total"]);
-    this.postdata = getJson.data.data;
-    this.currentpage = this.getQueryvalue('p');
-    this.formInline.input = decodeURIComponent(this.getQueryvalue('s'));
-    console.log(getJson);
-  },
+    },
+    methods: {
+        handleCurrentChange: function() {
+            const p = this.currentpage;
+            window.location.href = this.siteurl + "/search?s=" + this.getQueryvalue('s') + "&p=" + p;
+        },
+        getQueryvalue: function(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) {
+                return decodeURIComponent(r[2]);
+            }
+            return null;
+        },
+        getPostJson: async function() {
+            const e = this.getQueryvalue('s');
+            if (Number(this.getQueryvalue('p') == null)) {
+                var currentpage = 1
+            } else {
+                var currentpage = Number(this.getQueryvalue('p'));
+            }
+            this.formInline.input = e;
+            const a = await axios({
+                method: 'get',
+                url: this.siteurl + '/wp-json/gamux/v1/search',
+                params: {
+                    "page": currentpage,
+                    "per_page": 10,
+                    "search": e,
+                },
+            });
+            return a;
+        }
+    },
+    mounted: async function() {
+        this.siteurl = gamux.siteurl;
+        const getJson = await this.getPostJson();
+        this.total = Number(getJson["headers"]["x-wp-total"]);
+        this.postdata = getJson.data.data;
+        this.currentpage = this.getQueryvalue('p');
+        this.formInline.input = decodeURIComponent(this.getQueryvalue('s'));
+    },
 })
--- template/diy/search.php
+++ template/diy/search.php
@@ -5,25 +5,12 @@
 if (!defined('ABSPATH')) exit;
 get_header(); ?>
 
-<!--索引-->
-<section id="index">
-  <el-form :inline="true" :model="formInline" class="demo-form-inline">
-    <el-form-item>
-      <el-input v-model="formInline.input" placeholder="搜索一下"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">查询</el-button>
-    </el-form-item>
-  </el-form>
-</section>
-
-<el-divider></el-divider>
 <!--列表-->
 <section id="news">
     <el-card class="card" v-for="post in postdata" :key="post.id">
       <div class="game-info">
         <div class="game-name-sort">
-          <div class="game-name"><a :href="post.url">{{post.title}}</a></div>
+          <div class="game-name"><a :href="post.url">{{ post.title }}</a></div>
           <div class="game-sort">
             <span v-if="(post.exts.cats)[0]">{{ (post.exts.cats)[0].name }}</span>
             <span>{{ post.exts.modified }}</span>
