a82b97a91a92a6da28eb1928438c22aafdd2407e
小鸡 <345865759@163.com>
2022-09-07 23:56:38 +0800

    Update header.php

--- header.php
+++ header.php
@@ -19,8 +19,7 @@
 		?>
 	</title>
 	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/logo.ico">
-  <!-- <link crossorigin="anonymous" integrity="sha512-WYzwsHxfl8+qirF4V9ZyFULDBwxvm1kuCHlSd1F57JwALyQMxeq7j1blj4Y7pmP9r0tVMk7GYUI5xLDB7QIZCA==" href="https://lib.baomitu.com/element-ui/2.15.3/theme-chalk/index.min.css" rel="stylesheet"> -->
-  <link href="https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.3/theme-chalk/index.min.css" rel="stylesheet">
+  <link href="https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/element-ui/2.15.3/theme-chalk/index.min.css" type="text/css" rel="stylesheet" />
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   <?php if(is_single()) { ?>
